6de292473897b4cc32c476092e33fa0176de58f7
Jose Fernandez <joseferper@gmail.com>
2014-12-02 12:34:05 -0500

    Now dbs are installed using an array (more reusable)

--- puppet/modules/roles/manifests/db.pp
+++ puppet/modules/roles/manifests/db.pp
@@ -1,22 +1,26 @@
-class roles::db {
+class roles::db ($databases) {
 
   $mysql_host = '127.0.0.1'
 
   class { 'mysql::server': } ~>
   exec  { '/etc/init.d/mysql restart':
     refreshonly => true,
-  } ->
+  }
 
-  mysql::create_database { 'laiacapdevila':
-    db         => hiera('laiacapdevila::database::name'),
-    mysql_host => $mysql_host,
-  } ->
-  mysql::grant { 'laiacapdevila':
-    db         => hiera('laiacapdevila::database::name'),
-    user       => hiera('laiacapdevila::database::user'),
-    password   => hiera('laiacapdevila::database::password'),
-    mysql_host => $mysql_host,
-    hostname   => '%'
+  $databases.each |$database| {
+    mysql::create_database { $database['name']:
+      db         => hiera('laiacapdevila::database::name'),
+      mysql_host => $mysql_host,
+      require    => Class['mysql::server'],
+    } ->
+
+    mysql::grant { $database['name']:
+      db         => $database['name'],
+      user       => $database['user'],
+      password   => $database['password'],
+      mysql_host => $mysql_host,
+      hostname   => '%'
+    }
   }
 
 }
