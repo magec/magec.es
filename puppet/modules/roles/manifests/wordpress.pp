3d33202ac738551e4289dde2dd0c7b9eb53c21e2
Jose Fernandez <joseferper@gmail.com>
2014-12-03 06:04:22 -0500

    Solved some configuration issues

--- puppet/modules/roles/manifests/wordpress.pp
+++ puppet/modules/roles/manifests/wordpress.pp
@@ -8,8 +8,8 @@ class roles::wordpress ($blogs) {
   class { 'php5_mysql': } ->
   file { '/var/www/':
     ensure => 'directory',
-    owner => 'www-data',
-    mode  => '0755',
+    owner  => 'www-data',
+    mode   => '0755',
   }
   
   $blogs.each |$blog| {
@@ -23,6 +23,9 @@ class roles::wordpress ($blogs) {
       db_password    => $blog['db_password'],
       listen         => $listen,
       nginx_site     => true,
+      wp_lang        => $blog['wp_lang'],
+      version        => $blog['version'],
+      install_url    => $blog['install_url'],
       wp_site_domain => $blog['domain_name'],
       require        => File['/var/www'],
     } ->
